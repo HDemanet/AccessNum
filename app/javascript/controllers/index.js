a439bf3c582737cb0baffe5dc39831e40ca9559d
Helene Demanet <demanet.helene@gmail.com>
2025-02-19 18:34:04 +0100

    ok

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,5 +3,5 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 
-import SimulateController from "./simulate_controller"
+import SimulateController from "controllers/simulate_controller"
 application.register("simulate", SimulateController)
